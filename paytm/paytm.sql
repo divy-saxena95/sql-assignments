1f88ad3fd3163d72be1113f0a4db424b039b7ac6
Divy Saxena <divysaxena2@gmail.com>
2018-07-31 00:20:51 +0530

    modified schema

--- paytm/paytm.sql
+++ paytm/paytm.sql
@@ -1,22 +1,40 @@
 CREATE DATABASE IF NOT EXISTS paytm;
 USE paytm;
 
-CREATE TABLE IF NOT EXISTS customer (
-  customerID INT NOT NULL PRIMARY KEY,
-  name VARCHAR(45) ,
-  cust_type VARCHAR(45) ,
-  contactno VARCHAR(45),
-  branches_branchID INT NOT NULL REFERENCES branches(branchID) ON DELETE NO ACTION ON UPDATE CASCADE,
-  accounts_acct_number INT NOT NULL REFERENCES  accounts(acct_number) ON DELETE SET NULL ON UPDATE CASCADE);
-
-
 CREATE TABLE IF NOT EXISTS branches (
   branchID INT NOT NULL PRIMARY KEY,
   name VARCHAR(45) ,
   address VARCHAR(45) ,
   IFSCcode VARCHAR(45));
 
-CREATE TABLE IF NOT EXISTS accounts (
+CREATE TABLE IF NOT EXISTS accounts(
   acct_number INT NOT NULL PRIMARY KEY,
   type VARCHAR(45),
-  balance VARCHAR(45));
+  balance VARCHAR(45),
+  branches_branchID INT NOT NULL REFERENCES branches(branchID) ON DELETE SET NULL ON UPDATE CASCADE);
+CREATE TABLE IF NOT EXISTS customer (
+  customerID INT NOT NULL PRIMARY KEY,
+  name VARCHAR(45),
+  cust_type VARCHAR(45),
+  contactno VARCHAR(45),
+  accounts_acct_number INT NOT NULL REFERENCES accounts(acct_number) ON DELETE SET NULL ON UPDATE CASCADE);
+
+
+
+
+DELIMITER ///
+
+CREATE TRIGGER check_branch BEFORE INSERT ON customer FOR EACH ROW
+BEGIN
+IF ((SELECT account.acct_number
+   FROM accounts,branches,customer
+   WHERE accounts.acct_number = customer.acct_number AND accounts.branchID = Branches.branchID AND customer.customerID= NEW.customerID
+
+   AND Branch.branch_id IN (select branch_id from accounts where accounts.acct_number= NEW.acct_number)
+                        ) IS NOT NULL)
+THEN
+  UPDATE accounts SET acct_number = NULL WHERE acct_number = NEW.acct_number;
+  END IF;
+END;
+///
+DELIMITER ;
--- paytm/paytmtest.sql
+++ paytm/paytmtest.sql
@@ -1,16 +1,16 @@
 USE paytm;
  
- INSERT INTO customer VALUES(1,'Shivangi','Special','8967853421',1,2456);
- INSERT INTO customer VALUES(2,'Rahul','General','6737873828',2,2457);
- INSERT INTO customer VALUES(3,'Ranveer','VIP','437638483783',3,2458);
+ INSERT INTO customer VALUES(1,'Shivangi','Special','8967853421',1);
+ INSERT INTO customer VALUES(2,'Rahul','General','6737873828',2);
+ INSERT INTO customer VALUES(3,'Ranveer','VIP','437638483783',3);
 
  INSERT INTO branches VALUES(1,'CBO-1','Gandhi nagar','1234');
  INSERT INTO branches VALUES(2,'CBO-2','MG road','4567');
  INSERT INTO branches VALUES(3,'CBO-3','Arthur road','8901');
 
- INSERT INTO accounts VALUES(2456,'Savings','40000');
- INSERT INTO accounts VALUES(2457,'Current','20000');
- INSERT INTO accounts VALUES(2458,'Savings','10000');
+ INSERT INTO accounts VALUES(2456,'Savings','40000',2456);
+ INSERT INTO accounts VALUES(2457,'Current','20000',2457);
+ INSERT INTO accounts VALUES(2458,'Savings','10000',2458);
 
  UPDATE accounts SET balance='10000' WHERE balance='20000'; 
  UPDATE branches SET name='CBO-4' WHERE branchID=2; 
